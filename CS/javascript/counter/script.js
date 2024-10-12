7d07cca88eaa3b76bca33af34c48604f4aaac59c
AizenKurosaki <izeallangford16@gmail.com>
2024-10-12 10:45:46 -0400

    Update script.js

--- CS/javascript/counter/script.js
+++ CS/javascript/counter/script.js
@@ -1,3 +1,5 @@
 function counter() {
 // Write your code below. Hint: you can finish it in about 3-5 lines.
+  counter_number = document.getElementbyId('counter number');
+  counter-number.innerHTML = parsInt(counter_number)+1;
 }
